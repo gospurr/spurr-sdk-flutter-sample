772464f30f856877467274e7878ff0b6e700e8f2
Kazao TM <m.jumari@gmail.com>
2024-05-26 18:39:36 +0700

    - 0.1 complete

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,78 @@ class MainApp extends StatelessWidget {
     var partnerKey = "32158ebf-dd61-11ee-9ef9-42010a67a110";
     var spurr = new SDK(partnerId, partnerKey);
 
-    spurr.listLoyalty({})
-        .then((response) {
-            print('listLoyalty:response');
-            print(response);
-        })
-        .catchError((error) {
-            print('listLoyalty:error');
-            print(error);
-        });
+    // // spurr.setData('user03@yopmail.com', '+62811222003', 'ID', 'User', '03');
+    // spurr.setData('m.jumari@gmail.com', '+62811269110', 'ID', 'Kazao', 'TM');
+
+    // spurr.loyaltyAvailable({}).then((response) {
+    //   print('loyaltyAvailable:response');
+    //   if (response != null) {
+    //     for (var data in response['data']) {
+    //       print(data);
+    //     }
+    //   }
+    // }).catchError((error) {
+    //   print('loyaltyAvailable:error');
+    //   print(error);
+    // });
+
+    // spurr.loyaltyList({}).then((response) {
+    //   print('listLoyalty:response');
+    //   if (response != null) {
+    //     for (var data in response['data']) {
+    //       print(data);
+    //     }
+    //   }
+    // }).catchError((error) {
+    //   print('listLoyalty:error');
+    //   print(error);
+    // });
+
+    // spurr.loyaltyQrcode(
+    //     {'loyaltyId': 'a11c4341-0dec-4195-a1c0-f51d210af30e'}).then((response) {
+    //   print('loyaltyQrcode:response');
+    //   print(response);
+    //   spurr.qrcodeStatus(response['qrcodesId']).then((response) {
+    //     print('qrcodeStatus:response');
+    //     print(response);
+    //   }).catchError((error) {
+    //     print('qrcodeStatus:error');
+    //     print(error);
+    //   });
+    // }).catchError((error) {
+    //   print('loyaltyQrcode:error');
+    //   print(error);
+    // });
+
+    // spurr.campaignList({}).then((response) {
+    //   print('campaignList:response');
+    //   // print(response);
+    //   if (response != null) {
+    //     for (var data in response['data']) {
+    //       print(data);
+    //     }
+    //   }
+    // }).catchError((error) {
+    //   print('campaignList:error');
+    //   print(error);
+    // });
+
+    // spurr.campaignQrcode({
+    //   'promotionId': 'f628a457-190a-11ef-9ef9-42010a67a110'
+    // }).then((response) {
+    //   print('campaignQrcode:response');
+    //   print(response);
+    //   spurr.qrcodeStatus(response['qrcodesId']).then((response) {
+    //     print('qrcodeStatus:response');
+    //     print(response);
+    //   }).catchError((error) {
+    //     print('qrcodeStatus:error');
+    //     print(error);
+    //   });
+    // }).catchError((error) {
+    //   print('campaignQrcode:error');
+    //   print(error);
+    // });
 
     return const MaterialApp(
       home: Scaffold(
